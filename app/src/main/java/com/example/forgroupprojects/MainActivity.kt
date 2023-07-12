77afb0d521b9d02a5a0d026884b75d10ceabee2f
Reysaliot <kodegomd3p.rey.saliot@gmail.com>
2023-07-13 05:13:03 +0800

    Rey

--- app/src/main/java/com/example/forgroupprojects/MainActivity.kt
+++ app/src/main/java/com/example/forgroupprojects/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.forgroupprojects
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.example.forgroupprojects.R
 import com.example.forgroupprojects.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/forgroupprojects/SecondFragment.kt
+++ app/src/main/java/com/example/forgroupprojects/SecondFragment.kt
@@ -1,11 +1,12 @@
 package com.example.forgroupprojects
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.forgroupprojects.R
 import com.example.forgroupprojects.databinding.FragmentSecondBinding
 
 /**
